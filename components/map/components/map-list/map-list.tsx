da447d9893f486ededa0b3d0793284607eff6f1e
ezh3ee <ezh3ee@gmail.com>
2026-01-24 15:52:56 +0300

    анимация сайдбара

--- components/map/components/map-list/map-list.tsx
+++ components/map/components/map-list/map-list.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useSidebar } from "@/components/ui/sidebar";
 import { ArrowLeft } from "lucide-react";
+import { AnimatePresence, motion } from "motion/react";
 import { useState } from "react";
 import "./style.scss";
 
@@ -16,25 +17,43 @@ export default function MapList() {
 
   return (
     <div className="map-list-container">
-      <aside style={{ display: isVisible ? "block" : "none" }}>
-        <div className="search-container">
-          <input type="text" placeholder="Поиск по адресу" />
-          <button>Поиск</button>
-          <button>Очистить</button>
-        </div>
-        <div className="map-list">
-          <div className="map-list-item">
-            <div className="map-list-item-title">
-              <div className="map-list-item-title-text">
-                <div className="map-list-item-title-text-name">Москва</div>
-                <div className="map-list-item-title-text-address">
-                  Москва, ул. Большая Колесная, д. 10
+      <AnimatePresence mode="wait">
+        {/* <motion.aside style={{ maxWidth: isVisible ? "350px" : "0px" }}> */}
+        {isVisible && (
+          <motion.div
+            initial={{ opacity: 0, maxWidth: "0px" }}
+            animate={{ opacity: 1, maxWidth: "350px" }}
+            exit={{
+              opacity: 0,
+              maxWidth: "0px",
+              display: "none",
+              visibility: "hidden",
+            }}
+            transition={{
+              duration: 0.4,
+              maxWidth: { type: "spring", visualDuration: 0.1, bounce: 0.5 },
+            }}
+          >
+            <div className="search-container">
+              <input type="text" placeholder="Поиск по адресу" />
+              <button>Поиск</button>
+              <button>Очистить</button>
+            </div>
+            <div className="map-list">
+              <div className="map-list-item">
+                <div className="map-list-item-title">
+                  <div className="map-list-item-title-text">
+                    <div className="map-list-item-title-text-name">Москва</div>
+                    <div className="map-list-item-title-text-address">
+                      Москва, ул. Большая Колесная, д. 10
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-      </aside>
+          </motion.div>
+        )}
+      </AnimatePresence>
       <ArrowLeft className="arrow cursor-pointer" onClick={handleToggle} />
     </div>
   );
